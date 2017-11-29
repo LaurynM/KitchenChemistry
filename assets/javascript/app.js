5d70bf4d8bb959c88d94bcd4cc9ea4ac907c00ff
Lauryn McFarland <lauryn@lauryns-mbp.attlocal.net>
2017-11-28 20:58:04 -0600

    added game questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,29 @@
-var question = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-var answer = ['1!', '2!', '3!', '4!', '5!', '6!', '7!', '8!', '9!', '10!'];
+//Declare variables here that need to be available globally
+var questions = ["You are making a salad dressing. Which acid will you use?", 
+  "You are making a soup. Which solvent will you use?", 
+  "Time to season the food. Which salt will you use?", 
+  "You are mixing a cocktail. Which alcohol will you use?", 
+  "Bake a cake for dessert. Which ingredient will you use?", 
+  ];
+var choices = {
+  1: ["Formic Acid", "Nitrous Acid", "Acetic Acid"],
+  2: ["Benzene", "Dihydrogen Monoxide", "Acetone"],
+  3: ["Sodium Bisulfate", "Sodium Chloride", "Calcium Chloride"],
+  4: ["Methanol", "Propanol", "Ethanol"],
+  5: ["Cream of Tartar", "Milk of Magnesia", "Oil of Olay"],
+};
+var answers = {
+  "Acetic Acid" : "That's Vinegar. You made a tasty salad!", 
+  "Dihydrogen Monoxide" : "That's Water. You made a tasty soup!", 
+  "Sodium Chloride" : "That's Table Salt. You seasoned the food well!", 
+  "Ethanol" : "That's Drinking Alcohol. You made a tasty drink!", 
+  "Cream of Tartar" : "That's right! You made a tasty cake!", 
+};
+var time = 30; //30 seconds to read and answer each question
+var correct = 0;
+var incorrect = 0;
+
+
 // //  Step 2: do the following between the --- comments
 // //  after 5 seconds, execute the fiveSeconds function
 // //  after 10 seconds, execute the tenSeconds function
@@ -44,10 +68,14 @@ var answer = ['1!', '2!', '3!', '4!', '5!', '6!', '7!', '8!', '9!', '10!'];
 
 function newGame() {
   console.log("begin!");
+  //reset these variables
+  correct = 0;
+  incorrect = 0;
+
 }
 
 
 $( "#start" ).click(function() {
-  $( "#intro").hide();
+  $( "#intro" ).hide();
   newGame();
 });
